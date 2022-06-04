29d7ba822c775b66f97e94c435ac04cc2ccc052b
yamkaml <yamkamlionel@yahoo.fr>
2022-06-03 17:04:45 -0700

    use puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*this programme printf one message with puts fonction*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }

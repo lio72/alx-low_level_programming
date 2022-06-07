590c4d97812f221ae5b39d720f30d7d114efd1e9
yamkaml <yamkamlionel@yahoo.fr>
2022-06-07 12:47:35 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (sucess)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}

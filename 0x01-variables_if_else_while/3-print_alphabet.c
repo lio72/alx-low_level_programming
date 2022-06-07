581f36ed3ad9de0e3a8fc515fe87736ddaedd69e
yamkaml <yamkamlionel@yahoo.fr>
2022-06-06 18:32:36 -0700

    print laphabet with upcase

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (sucess)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		printf("%c", c);
+	for (c = 'A'; c <= 'Z'; c++)
+		printf("%c", c);
+	printf("\n");
+	return (0);
+}

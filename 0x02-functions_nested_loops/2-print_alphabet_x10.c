bf185a7460b94cf7d1f8424c3d30127ccb8b148b
yamkaml <yamkamlionel@yahoo.fr>
2022-06-08 16:19:18 -0700

    4552

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,19 +4,22 @@
 /**
  * print_alphabet_x10 - function to print string
  *
- * _putchar - write the character
+ * print_alphabet - write the character
  * Return: always 0 (sucess)
  */
 
 void print_alphabet_x10(void)
 {
-	char c;
 	int i;
+
+	char c;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for(c = 'a'; c <= 'z'; c++)
+		{
 			_putchar(c);
+		}
 		printf("\n");
 	}
 

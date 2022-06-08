dff40f48fb46d956f682ca8060e06414574d9883
yamkaml <yamkamlionel@yahoo.fr>
2022-06-08 07:45:11 -0700

    00011

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * main- Entry point
  *
  * Return: always 0 (sucess)
 */
-void print_alphabet(void)
-{
-	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
 }

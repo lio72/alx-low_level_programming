f3773936a4a53fceb77d0d4512857919db9e99de
yamkaml <yamkamlionel@yahoo.fr>
2022-06-08 18:02:18 -0700

    4555

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,11 +16,11 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		printf("\n");
+		_putchar("\n");
 	}
 
 }

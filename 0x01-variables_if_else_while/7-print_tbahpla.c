9af20076ed8f31db136c299e652b8db3a5abfc90
yamkaml <yamkamlionel@yahoo.fr>
2022-06-11 02:50:40 -0700

    print digit

---
+++ 0x01-variables_if_else_while/7-print_tbahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (sucess)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		printf("%c", c);
+	printf("\n");
+	return (0);
+}

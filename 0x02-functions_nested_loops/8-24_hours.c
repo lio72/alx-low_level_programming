284bda340276bc3f4cab72d31a5a982937934333
lionel72 <yamkamlionel@yahoo.fr>
2022-09-13 19:41:26 -0700

    24h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * jack_bauer - print last digit on one number
+ *
+ * Return: always valus (sucess)
+ *
+ */
+
+void jack_bauer(void)
+
+{
+	int h = 0;
+	int min = 0;
+	int h_tmp;
+	int min_temp;
+
+	while (h <= 23)
+	{
+		while (min <= 59)
+		{
+			h_tmp = h % 10;
+			_putchar(h / 10 + '0');
+			_putchar(h_tmp + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min_tmp + '0');
+			min++;
+			_putchar('\n');
+		}
+		h++;
+		min = 0;
+	}
+}

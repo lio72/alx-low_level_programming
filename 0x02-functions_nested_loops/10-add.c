4425ddac2a8f75195a9ec5a732ff1c7024023272
lionel72 <yamkamlionel@yahoo.fr>
2022-09-14 16:35:30 -0700

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _add- give absolu value of one number
+ *
+ * Return: always valus (sucess)
+ * @n1 : is parameter1
+ * @n2 : is parameter2
+ */
+
+int add(int n1, int n2);
+
+{
+	return (n1 + n2);
+}

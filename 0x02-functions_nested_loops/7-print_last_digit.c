65667e1157e4837687c0f8c5436cee30a35fc6d3
lionel72 <yamkamlionel@yahoo.fr>
2022-09-13 18:20:29 -0700

    last digit of our number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_last_digit print last digit on one number
+ *
+ * Return: always valus (sucess)
+ * @n : is parameter
+ */
+
+int print_last_digit(int n)
+
+{
+	return (n % 10);
+}

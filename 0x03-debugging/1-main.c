b6bef9669d5302ccc66b9fc10e55efdc99c18911
YDLJ <100246525+lio72@users.noreply.github.com>
2022-09-16 01:12:20 +0200

    Update 1-main.c
    comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,11 +8,9 @@
 int main(void)
 {
         int i;
-
-        printf("Infinite loop incoming :(\n");
-
-        i = 0;
         
+        printf("Infinite loop incoming :(\n");
+        i = 0;
         /**
 
         * while (i < 10)
@@ -21,8 +19,6 @@ int main(void)
         * }
         *  end of loop 
         */
-
         printf("Infinite loop avoided! \\o/\n");
-
         return (0);
 }

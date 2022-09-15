452f9fd48105e54bfbc47682e58a708986b182f3
lionel72 <yamkamlionel@yahoo.fr>
2022-09-14 17:26:33 -0700

    pint to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 -  give absolu value of one number
+ *
+ * Return: always valus (sucess)
+ * @n : is parameter
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+		}
+	}
+	else
+	{
+		for (i = n; i >=  98; i--)
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}

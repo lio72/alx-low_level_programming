095fe38d21aa53ae320d9c20c9d0afdbf6d98a94
lionel72 <yamkamlionel@yahoo.fr>
2022-09-15 18:36:23 -0700

    print most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - fuction test if enter is digit
+ *
+ *
+ * Return: always 0 (sucess)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			pass;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+
+}

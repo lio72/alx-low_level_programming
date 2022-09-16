1b19378b3c13edf4d595ccd1cb620ee7c2ddf63d
lionel72 <yamkamlionel@yahoo.fr>
2022-09-15 17:57:50 -0700

    nul

---
+++ 0x04-more_functions_nested_loops/2-nul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * nul - fuction test if enter is digit
+ *
+ *@a : is parameter1
+ *@b : is parameter2
+ * Return: always 0 (sucess)
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}

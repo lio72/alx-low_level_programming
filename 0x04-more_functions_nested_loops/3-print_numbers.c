cd009cff8d1681474f8d53914d3bfdd6ee467ad8
lionel72 <yamkamlionel@yahoo.fr>
2022-09-15 18:24:04 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - fuction test if enter is digit
+ *
+ *
+ * Return: always 0 (sucess)
+ */
+
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+}

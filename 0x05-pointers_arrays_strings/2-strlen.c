3eabdde07e4fd7580b0d494461d667b96d98317e
lionel72 <yamkamlionel@yahoo.fr>
2022-09-20 15:57:59 -0700

    len of character

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - is function to update values
+ *
+ * 
+ * @s : is parameter
+ * return 0
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

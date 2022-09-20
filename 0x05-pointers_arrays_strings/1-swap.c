b12514f53f9037eb27e249598c801bd66fa797ab
lionel72 <yamkamlionel@yahoo.fr>
2022-09-20 15:04:28 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - is function to update values
+ *
+ * @a : is parameter
+ * @b : is parameter
+ * return 0
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+
+
+}

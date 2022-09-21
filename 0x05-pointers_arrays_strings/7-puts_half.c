318ffdef725b56cc1eea380c99f5080dd0d7b15e
YDLJ <100246525+lio72@users.noreply.github.com>
2022-09-21 03:37:33 +0200

    Update 7-puts_half.c
    print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,33 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
  */
-void _puts(char *str)
+void puts_half(char *str)
 {
-	int i;
+	int len, n, i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		_putchar(str[i]);
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }

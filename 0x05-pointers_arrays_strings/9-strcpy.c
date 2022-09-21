fbdc10d34d54f9a8e5ad8b7abdbe3d81b23e4937
YDLJ <100246525+lio72@users.noreply.github.com>
2022-09-21 03:39:57 +0200

    Update 9-strcpy.c
    use str copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,30 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
  */
-void _puts(char *str)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int len, i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	len = 0;
+
+	while (src[len] != '\0')
 	{
-		_putchar(str[i]);
+		len++;
 	}
-	_putchar('\n');
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }

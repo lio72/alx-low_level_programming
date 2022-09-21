10b992afca0e1e058aa90a69f4eb73fa1ff95223
YDLJ <100246525+lio72@users.noreply.github.com>
2022-09-21 03:38:52 +0200

    Update 8-print_array.c
    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array to be printed
+ * @n: number of elements to print
  */
-void _puts(char *str)
+void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
-		_putchar(str[i]);
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 	}
-	_putchar('\n');
+		printf("\n");
 }

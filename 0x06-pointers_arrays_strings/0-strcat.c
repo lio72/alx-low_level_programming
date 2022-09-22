ad03fc373c68f1bda366b3768372241e100e3a10
YDLJ <100246525+lio72@users.noreply.github.com>
2022-09-22 03:35:55 +0200

    Update 0-strcat.c
    str copy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,43 +1,24 @@
 #include "main.h"
-
 /**
- * _strcat - this is a funcion strcat
- * @dest: first param
- * @src: second param
- * Return: a pointer to the destination string dest
+*_strcat - function that concatenates two strings.
+*@dest: pointer to destination string.
+*@src: pointer to source string.
+*
+*Return: pointer to destination string.
  */
 char *_strcat(char *dest, char *src)
 {
-char *dest_end = dest;
-int src_len = 0;
+int length, j;
 
-while (*dest_end)
-++dest_end;
-
-<<<<<<< HEAD
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	for (k = 0; src[k] != '\0'; k++)
-	{
-		i = i + 1;
-		dest[i] = src[k];
-	}
-	dest[i] = '\0';
-=======
-while (src[src_len])
-++src_len;
->>>>>>> 9454a7019ebfbff2da4bddff7ab94dc1c0538ad9
-
-if (src + src_len < dest || dest_end + src_len < src)
+length = 0;
+while (dest[length] != '\0')
 {
-do {
-*dest_end++ = 
-*src++;
-} while (src_len--);
-
+length++;
 }
+for (j = 0; src[j] != '\0'; j++, length++)
+{
+dest[length] = src[j];
+}
+dest[length] = '\0';
 return (dest);
-
 }

9454a7019ebfbff2da4bddff7ab94dc1c0538ad9
YDLJ <100246525+lio72@users.noreply.github.com>
2022-09-22 03:23:40 +0200

    Update 0-strcat.c
    str cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,30 @@
 #include "main.h"
+
 /**
  * _strcat - this is a funcion strcat
  * @dest: first param
  * @src: second param
  * Return: a pointer to the destination string dest
  */
-
-char *_strcat(int *dest, int *src)
+char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int k;
+char *dest_end = dest;
+int src_len = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	for (k = 0; src[k] != '\0'; k++)
-	{
-		i = i + 1;
-		dest[i] = src[k];
-	}
+while (*dest_end)
+++dest_end;
 
-	return (dest);
+while (src[src_len])
+++src_len;
+
+if (src + src_len < dest || dest_end + src_len < src)
+{
+do {
+*dest_end++ = 
+*src++;
+} while (src_len--);
+
+}
+return (dest);
 
 }

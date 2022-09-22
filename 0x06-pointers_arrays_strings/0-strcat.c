98e3232675b3328d1fd0b3ec1eec8ea13110b806
lionel72 <yamkamlionel@yahoo.fr>
2022-09-21 17:09:58 -0700

    strcar 200

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,11 +15,11 @@ char *_strcat(int *dest, int *src)
 	int i = 0;
 	int k;
 
-	while (dest[i])
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for (k = 0; src[k]; k++)
+	for (k = 0; src[k] != '\0'; k++)
 	{
 		dest[i] = src[k];
 		i++;

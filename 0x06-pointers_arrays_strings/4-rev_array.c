b4aac09b918b45130fb373014c9e94a4ef9e959f
lionel72 <yamkamlionel@yahoo.fr>
2022-09-22 18:16:46 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _reverse_array - function that reverse array.
+ *
+ * @a: is the parmeter1.
+ * @n: is the parameter2.
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n);
+
+{
+	int memo, i;
+
+	for (i = n - 1; i > 0 ; i--)
+	{
+		memo = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] =  memo;
+	}
+}

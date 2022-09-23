240159015dc221cc389f99d9c8dc6de8cf570647
lionel72 <yamkamlionel@yahoo.fr>
2022-09-22 17:44:15 -0700

    lio wok

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int val, i;
 
-	while (s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0'))
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
 		i++;
 	}
@@ -38,10 +38,5 @@ int _strcmp(char *s1, char *s2)
 		val = s1[i];
 		break;
 	}
-	else
-	{
-
-	}
-
 	return (val);
 }

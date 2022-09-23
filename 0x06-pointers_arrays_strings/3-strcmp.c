0e50c7a48923b13877c29b5a66d53d80aa5ceb5e
lionel72 <yamkamlionel@yahoo.fr>
2022-09-22 20:46:16 -0700

    strong

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -22,22 +22,18 @@ int _strcmp(char *s1, char *s2)
 	if (s1[i] == '\0' && s2[i] == '\0')
 	{
 		val = 0;
-		break;
 	}
 	else if (s1[i] > s2[i] || s1[i] < s2[i])
 	{
 		val = s1[i] - s2[i];
-		break;
 	}
 	else if (s1[i] == '\0')
 	{
-		val = s2[i];
-		break;
+		val = 0 - s2[i];
 	}
 	else if (s2[i] == '\0')
 	{
-		val = s1[i];
-		break;
+		val = 0 + s1[i];
 	}
 
 	return (val);

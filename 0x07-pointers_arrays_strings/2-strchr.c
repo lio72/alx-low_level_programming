e89fc4805c9e393014029da53cf2b1e4b82f4f10
lionel72 <yamkamlionel@yahoo.fr>
2022-09-27 16:47:04 -0700

    lio2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,13 @@
 char *_strchr(char *s, char c);
 
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (s);
-			break;
+			return (s + i);
 		}
 
 	}

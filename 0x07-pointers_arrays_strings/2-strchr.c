4d2ff1221fca5e4988f1e07a1c90e0327debffe2
lionel72 <yamkamlionel@yahoo.fr>
2022-09-27 16:28:36 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strchr - function return firt occurence of @c.
+* @s: is parameter1.
+* @c: is parameter2.
+*
+* Return: pointer to destination string.
+*/
+
+char *_strchr(char *s, char c);
+
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s);
+			break
+		}
+
+	}
+	return (NULL);
+
+}

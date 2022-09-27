3b94df1fffec83027d9f6807145de639bf650112
lionel72 <yamkamlionel@yahoo.fr>
2022-09-27 14:46:41 -0700

    cpy octect

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _memset - function that copy in @n fist octec of mémory @b.
+* @s: is parameter1.
+* @b: is parameter2.
+* @n: is parameter3.
+*
+* Return: pointer to destination string.
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) =  b;
+	}
+
+	return (s);
+
+}

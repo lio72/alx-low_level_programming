3e2aef54280a3138dcebb301c4eb765e9fe3544e
lionel72 <yamkamlionel@yahoo.fr>
2022-09-27 17:31:50 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_chessboard - function return firt occurence of @c.
+* @a: is parameter1.
+*
+* Return: pointer to destination string.
+*/
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+	}
+	return (NULL);
+
+}

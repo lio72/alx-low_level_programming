28a0bec2fadb9513c8b891ed47a26bf027b2e760
lionel72 <yamkamlionel@yahoo.fr>
2022-09-27 18:02:03 -0700

    set str

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* set_string - function that set string  @s.
+* @s: is parameter1.
+* @to: is parameter2.
+*
+* Return: pointer to destination string.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

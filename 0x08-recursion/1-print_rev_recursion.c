9aad485bbb555c8357746ab6b2a73fe0ef83c518
YDLJ <100246525+lio72@users.noreply.github.com>
2022-09-30 00:32:18 +0200

    Update 1-print_rev_recursion.c
    print rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,15 +8,11 @@
 */
 
 void _print_rev_recursion(char *s)
-
 {
-	if (*s ==  '\0')
-	{
-		_putchar(*s);
-
-	}
-	else
-	{
-		_putchar('\n');
-	}
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }

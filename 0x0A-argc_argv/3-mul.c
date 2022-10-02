d72eec88e3c7583b5197edd9e238bc4b32f69328
lionel72 <yamkamlionel@yahoo.fr>
2022-10-01 18:15:43 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - is the principal programme to printf.
+ * @argc: is argument1.
+ * @argv: is argument2.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+	}
+
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+	}
+	return (0);
+
+}

86501f2f92a15880a74f10339a14c90b5c25a593
YDLJ <100246525+lio72@users.noreply.github.com>
2022-10-03 21:49:11 +0200

    Update 4-add.c
    4 add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,85 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - is the principal programme to printf.
- * @argc: is argument1.
- * @argv: is argument2.
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: 0.
+ * Return: the int converted from the string
  */
-
-int main(int argc, char **argv)
+int _atoi(char *s)
 {
-	int i, a, s = 0;
+	int i, d, n, len, f, digit;
 
-	if (argc == 1)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		printf("0\n");
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			a = atoi(argv[i]);
-			s = s + a;
-		}
-		else
-		{
-			printf("Error\n");
-			break;
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
 		}
 	}
-	printf("%d\n", s);
 
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
 	return (0);
-
 }

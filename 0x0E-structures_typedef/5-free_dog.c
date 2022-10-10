a58c2fdb7c022d4687e92c13798645610a5fe2b5
YDLJ <100246525+lio72@users.noreply.github.com>
2022-10-11 01:42:42 +0200

    Update 5-free_dog.c
    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,33 +1,17 @@
-#include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
-
-
+#include "dog.h"
 /**
-* print_dog - is function for init struct
-*
-* @d: is struct1
-*
-*/
-
-void print_dog(struct dog *d)
-
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d)
 	{
-		if (d.name != NULL && d.age != NULL && d.owner != NULL)
-		{
-			printf("Name: %s\n", d.name);
-			printf("Age: %f\n", d.age);
-			printf("Owner: %s\n", d.owner);
-		}
-		else if (d.name == NULL || d.age == NULL || d.owner == NULL)
-		{
-			printf("nil\n");
-		}
-	}
-
-	else
-	{
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }

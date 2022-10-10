a3a10aa8a2279bdda7eb43e2371e7955bb98bbc6
lionel72 <yamkamlionel@yahoo.fr>
2022-10-10 14:40:49 -0700

    struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+0x0E. C - in this task, we trie to use struct type
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+* struct dog - is struct to describe dog propriety.
+* @name: parameter1.
+* @age: parameter2.
+* @owner: parameter3
+*
+*/
+
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

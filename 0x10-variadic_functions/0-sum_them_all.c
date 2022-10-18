1231be08ae328150ab3d6d85a51898443e4b1d90
lionel72 <yamkamlionel@yahoo.fr>
2022-10-18 04:48:25 -0700

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - is a variadic function to return sum of all parameter.
+ *
+ * @n: is initalized parameter.
+ *
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i;
+	va_list mylist;
+
+	va_start(mylist, n);
+
+	if (n > 0)
+	{
+		for (i = 0; i < 0; i++)
+		{
+			sum += va_arg(mylist, unsigned int);
+
+		}
+		va_end(mylist);
+		return (sum);
+	}
+	return (0);
+
+
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,7 @@
+0x10. C - how to use variadic functionc 
+variadic function help us to use undeterninate number of varible in our function
+for use it, it is necessary for us to know to use
+va_list : to call pointer on arg
+va_end : to all and initalize our pointer
+va_arg : firt value of our pointer
+va_end : end of our arg list.

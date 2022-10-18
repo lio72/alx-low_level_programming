9154b45837f7a9c07a6c19a9e9e703f9f1fc9513
lionel72 <yamkamlionel@yahoo.fr>
2022-10-18 05:30:41 -0700

    print parameter

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - is the function that can print all parameneter.
+ *
+ * @separator: is initalized parameter.
+ * @n: second parameter.
+ *
+ * Return: nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+
+{
+	unsigned int i;
+	va_list mylist;
+
+	va_start(mylist, separator);
+
+	if (separator)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", a_arg(mylist, unsigned int));
+
+		}
+		va_end(mylist);
+	}
+	printf("\n");
+	return (0);
+
+
+}

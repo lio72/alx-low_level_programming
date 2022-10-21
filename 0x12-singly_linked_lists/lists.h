b70751b6c93505dbd813e23746fdb2b683d62e1f
YDLJ <100246525+lio72@users.noreply.github.com>
2022-10-21 04:57:01 +0200

    Update lists.h
    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -10,7 +13,6 @@
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -18,7 +20,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif

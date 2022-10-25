8cf6ebc3c872367e15b12f57df526553f20eed6e
lionel72 <yamkamlionel@yahoo.fr>
2022-10-24 19:11:45 -0700

    list linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *print_listint - print list of our fuction
+ *
+ * @h: is our list
+ * Return: number
+ *
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		number += 1
+	}
+	return (number);
+}

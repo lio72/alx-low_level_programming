71fb7f6a3010c46a3469bcdba9ad7c742682e36d
lionel72 <yamkamlionel@yahoo.fr>
2022-11-14 17:23:47 -0800

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,12 +12,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 
 {
-	if (index > (sizeof(unsigned long int) * 8) - 1)
+	if (index > (sizeof(n) * 8) - 1)
 	{
 		return (-1);
 	}
 
-	if (n & (1 << index) == 0)
+	if (n & ((1 << index)) == 0)
 	{
 		return (0);
 	}

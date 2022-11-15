8054433e44b8db91f4f0f09df75e494dad3daa85
lionel72 <yamkamlionel@yahoo.fr>
2022-11-14 19:38:18 -0800

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - is function to set bit with index that it have
+ *
+ * @n: is a parameter.
+ * @m: is a parameter2
+ *
+ * Return: values 0 or 1
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+{
+	unsigned long int xor, bit;
+
+	xor = n ^ m;
+	bit =  0;
+	while (xor > 0)
+	{
+		bit = bit + (xor & 1);
+		xor >> 1;
+	}
+	return (bit);
+}

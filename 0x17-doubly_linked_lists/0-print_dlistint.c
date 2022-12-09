584b5f2510db76d34ec22465b157605eddf7ca7b
lionel72 <yamkamlionel@yahoo.fr>
2022-12-08 18:21:20 -0800

    print number of node

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * print_dlistint - is the function to print elemen of dlist
+ * @h: is our dlist
+ *
+ * Return: number of node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,2 @@
+0x13. C - more linked list, we are going to use the linked list.
+in this task, we are going to upgrade our knowledge on the linkeds lists
